7852e30e56fd7dfbcbb03fa7c9dbc27ad0e10525
Nate Rauh <NRauh@users.noreply.github.com>
2016-09-22 18:32:38 -0500

    Made changes to functions format and added scripts
    Enqueued scripts and changed the formatting for the setup function.
    Also changed the class for pagination.

--- functions.php
+++ functions.php
@@ -1,18 +1,30 @@
 <?php
 
-if (!function_exists('dsmworkers_setup')):
+if (!function_exists('dsmworkers_setup')) {
+	function dsmworkers_setup() {
+		add_theme_support('post-thumbnails');
+		add_theme_support('title-tag');
+		add_theme_support('custom-logo');
 
-function dsmworkers_setup() {
-	add_theme_support('post-thumbnails');
-	add_theme_support('title-tag');
-	add_theme_support('custom-logo');
-
-	register_nav_menus(array(
-		'primary' => __('Header Menu')
-	));
+		register_nav_menus(array(
+			'primary' => __('Header Menu')
+		));
+	}
 }
-
-endif;
-
 add_action('after_setup_theme', 'dsmworkers_setup');
+
+function dsmworkers_scripts() {
+	wp_enqueue_script('dsmworkers', get_template_directory_uri() . '/js/dsmworkers.js', null, 1.0, false);
+}
+add_action('wp_enqueue_scripts', 'dsmworkers_scripts');
+
+function next_posts_attributes() {
+	return 'class="next"';
+}
+function previous_posts_attributes() {
+	return 'class="prev"';
+}
+add_filter('next_posts_link_attributes', 'next_posts_attributes');
+add_filter('previous_posts_link_attributes', 'previous_posts_attributes');
+
 ?>
